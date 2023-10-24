1caa302fc556e8133eff7760027e3c476235f74f
Vijayakumar-Periyasamy <vijayrose1053@gmail.com>
2023-10-24 20:51:44 +0530

    base code setup is done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {RazorpayComponent} from './razorpay/razorpay.component'
+import {HomeComponent} from './home/home.component'
 
 const routes: Routes = [
   {
     path: '',
-    component: RazorpayComponent, data: {pageName: 'Razorpay'}
+    component: HomeComponent, data: {pageName: 'Home'}
+},
+{
+  path: 'razorpay',
+  component: RazorpayComponent, data: {pageName: 'Razorpay'}
 }
 ];
 

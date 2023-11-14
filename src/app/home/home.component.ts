95c7832113f79f7efee1088f3f975a7c999e4df5
Anto Vasanth T <antovasanth.t@dataflo.io>
2023-11-14 18:05:19 +0530

    Added New Sections

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,4 +7,56 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  divone:boolean = true;
+  divtwo:boolean = false;
+  divthree:boolean = false;
+
+  faqone = false;
+  faqtwo = false;
+  faqthree = false;
+  faqfour = false;
+
+  constructor(){}
+
+  showslider(divname: string) {
+    console.log("div name : ", divname);
+    if(divname === "one") {
+      this.divone = true;
+      this.divtwo = false;
+      this.divthree = false;
+    } else if(divname === "two") {
+      this.divone = false;
+      this.divtwo = true;
+      this.divthree = false;
+    } else if(divname === "three") {
+      this.divone = false;
+      this.divtwo = false;
+      this.divthree = true;
+    }
+  }
+
+  faqclicked(faqname: string) {
+    console.log("div name : ", faqname);
+    if(faqname === "one") {
+      this.faqone = true;
+      this.faqtwo = false;
+      this.faqthree = false;
+      this.faqfour = false;
+    } else if(faqname === "two") {
+      this.faqtwo = true;
+      this.faqone = false;
+      this.faqthree = false;
+      this.faqfour = false;
+    } else if(faqname === "three") {
+      this.faqthree = true;
+      this.faqtwo = false;
+      this.faqone = false;
+      this.faqfour = false;
+    } else if(faqname === "four") {
+      this.faqfour = true;
+      this.faqtwo = false;
+      this.faqthree = false;
+      this.faqone = false;
+    } 
+  }
 }

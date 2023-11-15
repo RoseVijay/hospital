164ac520d4ef1f4ba1aad49805a5c85e7e1d4df0
Vijayakumar-Periyasamy <vijayrose1053@gmail.com>
2023-11-15 20:07:53 +0530

    header footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RazorpayComponent } from './razorpay/razorpay.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    RazorpayComponent
+    RazorpayComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

cb635305715a002de3d191b4a453427611aa943e
Anto Vasanth T <antovasanth.t@dataflo.io>
2023-11-16 17:44:21 +0530

    Added sub page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {RazorpayComponent} from './razorpay/razorpay.component'
 import {HomeComponent} from './home/home.component'
+import { ParvathyhomecareComponent } from './parvathyhomecare/parvathyhomecare.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent, data: {pageName: 'Home'}
-},
-{
-  path: 'razorpay',
-  component: RazorpayComponent, data: {pageName: 'Razorpay'}
-}
+  },
+  {
+    path: 'razorpay',
+    component: RazorpayComponent, data: {pageName: 'Razorpay'}
+  },
+  { 
+    path: 'parvathyhomecare',
+    component: ParvathyhomecareComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { RazorpayComponent } from './razorpay/razorpay.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ParvathyhomecareComponent } from './parvathyhomecare/parvathyhomecare.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FooterComponent } from './footer/footer.component';
     HomeComponent,
     RazorpayComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ParvathyhomecareComponent
   ],
   imports: [
     BrowserModule,

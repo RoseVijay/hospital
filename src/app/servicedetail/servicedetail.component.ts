cb7a630cd9c4e97af3987ec77fc2803584f5f80b
Anto Vasanth T <antovasanth.t@dataflo.io>
2023-11-28 00:44:09 +0530

    button changes

--- src/app/servicedetail/servicedetail.component.ts
+++ src/app/servicedetail/servicedetail.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./servicedetail.component.scss']
 })
 export class ServicedetailComponent {
+  servicelist: boolean = false;
+  aboutmoreless: boolean = false;
+  faqone: boolean = false;
+  faqtwo: boolean = false;
+  faqthree: boolean = false;
+  faqfour: boolean = false;
+  faqfive: boolean = false;
 
+  showtextlist() {
+    this.servicelist = !this.servicelist;
+  }
+
+  readmore() {
+    this.aboutmoreless = true;
+  }
+
+  readless() {
+    this.aboutmoreless = false;
+  }  
+
+  showfaqans(faqname: string) {
+    console.log("div name : ", faqname);
+    if (faqname === "one") {
+      this.faqone = !this.faqone;
+    }
+    if (faqname === "two") {
+      this.faqtwo = !this.faqtwo;
+    }
+    if (faqname === "three") {
+      this.faqthree = !this.faqthree;
+    }
+    if (faqname === "four") {
+      this.faqfour = !this.faqfour;
+    }
+    if (faqname === "five") {
+      this.faqfive = !this.faqfive;
+    }
+  }
 }

68ca5e5c2ac60a4faa30987b564b699297835796
anto2126 <vasni2126@gmail.com>
2024-02-11 14:40:02 +0530

    Added Login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { CaregiverforelderlyComponent } from './caregiverforelderly/caregiverfor
 import { HomexrayComponent } from './homexray/homexray.component';
 import { ServicedetailComponent } from './servicedetail/servicedetail.component';
 import { AllservicesComponent } from './allservices/allservices.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
@@ -71,6 +72,10 @@ const routes: Routes = [
   {
     path: 'allservices',
     component: AllservicesComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { CaregiverforelderlyComponent } from './caregiverforelderly/caregiverfor
 import { HomexrayComponent } from './homexray/homexray.component';
 import { ServicedetailComponent } from './servicedetail/servicedetail.component';
 import { AllservicesComponent } from './allservices/allservices.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -38,7 +39,8 @@ import { AllservicesComponent } from './allservices/allservices.component';
     CaregiverforelderlyComponent,
     HomexrayComponent,
     ServicedetailComponent,
-    AllservicesComponent
+    AllservicesComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
